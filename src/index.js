2fe2139d71e9995d646a3f71319e700a4db2a08f
Andrey Popov <andpop@mail.ru>
2018-10-25 15:47:15 +0300

    Work1, tasks 1,2.

--- src/index.js
+++ src/index.js
@@ -36,6 +36,69 @@ function sumWithDefaults(a, b) {
     return a + b;
 }
 
-/* eslint-disable no-console */
-returnFirstArgument(10);
-console.log(sumWithDefaults(10));
+/*
+ Задание 3:
+
+ Функция должна принимать другую функцию и возвращать результат вызова этой функции
+
+ Пример:
+   returnFnResult(() => 'привет') вернет 'привет'
+ */
+function returnFnResult(fn) {
+}
+
+/*
+ Задание 4:
+
+ Функция должна принимать число и возвращать новую функцию (F)
+ При вызове функции F, переданное ранее число должно быть увеличено на единицу и возвращено из F
+
+ Пример:
+   var f = returnCounter(10);
+
+   console.log(f()); // выведет 11
+   console.log(f()); // выведет 12
+   console.log(f()); // выведет 13
+ */
+function returnCounter(number) {
+}
+
+/*
+ Задание 5 *:
+
+ Функция должна возвращать все переданные ей аргументы в виде массива
+ Количество переданных аргументов заранее неизвестно
+
+ Пример:
+   returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
+ */
+function returnArgumentsArray() {
+}
+
+/*
+ Задание 6 *:
+
+ Функция должна принимать другую функцию (F) и некоторое количество дополнительных аргументов
+ Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
+
+ Пример:
+   function sum(a, b) {
+     return a + b;
+   }
+
+   var newSum = bindFunction(sum, 2, 4);
+
+   console.log(newSum()) выведет 6
+ */
+function bindFunction(fn) {
+}
+
+export {
+    returnFirstArgument,
+    sumWithDefaults,
+    returnArgumentsArray,
+    returnFnResult,
+    returnCounter,
+    bindFunction
+}
+
