3b1384c124d97bd141709db8b14b8024ff0828fe
Andrey Popov <andpop@mail.ru>
2018-10-30 22:20:58 +0300

    Work 2, tasks 1-4.

--- src/index.js
+++ src/index.js
@@ -56,6 +56,10 @@ function reduce(array, fn, initial) {
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
+    const properties = Object.keys(obj);
+    const upperProps = properties.map(value => value.toUpperCase());
+
+    return upperProps;
 }
 
 /*
